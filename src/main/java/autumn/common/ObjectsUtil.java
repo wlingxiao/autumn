d2a915c0e5325048fc048ea26bbcc18f7f7c6055
wlingxiao <wlingxiao@outlook.com>
2017-08-26 16:07:30 +0800

    Revert "增加 mysql 支持"
    This reverts commit 659151e58c811788954beb72ba94ccf6a1731665.

--- src/main/java/autumn/common/ObjectsUtil.java
+++ src/main/java/autumn/common/ObjectsUtil.java
@@ -2,7 +2,7 @@ package autumn.common;
 
 import java.util.Objects;
 
-public final class ObjectsUtil {
+public class ObjectsUtil {
 
     public static <T> boolean equal(T a, T b) {
         return Objects.equals(a, b);
