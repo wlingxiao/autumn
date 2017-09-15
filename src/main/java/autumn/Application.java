12e7da0e41918e63b7e38ae6f70c5a81cc6ab168
wlingxiao <wlingxiao@outlook.com>
2017-09-15 16:26:24 +0800

    enable spring cache

--- src/main/java/autumn/Application.java
+++ src/main/java/autumn/Application.java
@@ -3,12 +3,14 @@ package autumn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableAsync
+@EnableCaching
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/autumn/post/support/PostRepository.java
+++ src/main/java/autumn/post/support/PostRepository.java
@@ -1,11 +1,15 @@
 package autumn.post.support;
 
 import autumn.post.Post;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestBody;
 
+
+@CacheConfig(cacheNames = "post")
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    @Cacheable
     Post findById(final Long id);
 }
